0ccdceb60664a4a22db90357823eac52116044e9
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-03-09 09:34:40 +0100

    Created firstgame

---
+++ src/at/seya/games/firstgame/Rectangles.java
@@ -0,0 +1,25 @@
+package at.seya.games.firstgame;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+import javax.swing.plaf.basic.BasicArrowButton;
+
+public class Rectangles extends BasicGame {
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
