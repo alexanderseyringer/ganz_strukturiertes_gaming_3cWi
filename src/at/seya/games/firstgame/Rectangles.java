ea6a91ab49e3ec4446cdcda5cbfdd766d5208e48
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-03-11 11:15:58 +0100

    Started with Assignemnt 01 - firstgame

--- src/at/seya/games/firstgame/Rectangles.java
+++ src/at/seya/games/firstgame/Rectangles.java
@@ -1,25 +1,38 @@
 package at.seya.games.firstgame;
 
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
+import org.newdawn.slick.*;
+import org.newdawn.slick.tests.AnimationTest;
 
 import javax.swing.plaf.basic.BasicArrowButton;
 
 public class Rectangles extends BasicGame {
+    public Rectangles(String title) {
+        super(title);
+    }
+
     @Override
-    public void init(GameContainer gameContainer) throws SlickException {
+    public void init(GameContainer gameContainer) throws SlickException { // First things
 
     }
 
     @Override
-    public void update(GameContainer gameContainer, int i) throws SlickException {
+    public void update(GameContainer gameContainer, int i) throws SlickException { // Bewegungen
 
     }
 
     @Override
-    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException { // Was dargestellt werden soll
+        graphics.drawRect(100,100,100,100);
+        graphics.drawString();
+    }
 
+    public static void main(String[] argv) {
+        try {
+            AppGameContainer container = new AppGameContainer(new Rectangles("Squared Rectangles"));
+            container.setDisplayMode(800,600,false);
+            container.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
     }
 }
